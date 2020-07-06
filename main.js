d43825da3242664dac5164eba42a748477dd0298
Christina <christinajoshi1@gmail.com>
2020-07-06 12:33:53 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/* ESLint is a static code analysis tool for identifying problematic patterns found in JavaScript code.  */
+jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 
+global $, window, document 
+global document
+
+// Simple jQuery event handler
+$(document).ready(function () {
+   // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+});
+
