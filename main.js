6d76c1c9c2638bd7c94943c58e1aa82a09f23847
Christina <christinajoshi1@gmail.com>
2020-07-06 21:46:26 -0400

    Modal is working, but too large for screen

--- main.js
+++ main.js
@@ -2,19 +2,19 @@
  * Auto-generated content from the Brackets New Project extension.
  */
 
-/* ESLint is a static code analysis tool for identifying problematic patterns found in JavaScript code.  */
+/* ESLint is a static code analysis tool for identifying problematic patterns found in JavaScript code. 
 jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 
 global $, window, document 
-global document
+global document  */
 
 // Simple jQuery event handler
 $(document).ready(function () {
    // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("modal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
+var img = document.getElementById("featureImage");
+var modalImg = document.getElementById("featureImagePop");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
